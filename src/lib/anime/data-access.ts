888b8a992f80b0b23d5e2d6f447242112e0e19d3
JesusP2 <jesus_perez99@hotmail.com>
2025-01-30 02:48:04 -0600

    added anime views

--- src/lib/anime/data-access.ts
+++ src/lib/anime/data-access.ts
@@ -1,4 +1,4 @@
-import { count, eq } from "drizzle-orm";
+import { and, count, eq } from "drizzle-orm";
 import { animeTable, trackedEntityTable } from "../db/schemas";
 import { animeSearchParamsToDrizzleQuery } from "./searchparams-to-drizzle";
 import { db } from "../db/pool";
@@ -9,7 +9,7 @@ import type { AnimeCardItem } from "@/components/anime-card";
 import { ok, err, type Result } from "neverthrow";
 import { cleanSearchParams } from "../utils/clean-searchparams";
 
-export async function getAnimesCount(
+export async function getCurrentSeasonCount(
   searchParams: URLSearchParams,
   recordsPerPage = 25,
 ): Promise<Result<number, Error>> {
@@ -41,6 +41,92 @@ export async function getCurrentSeason(
     cleanedSearchParams,
     recordsPerPage,
   );
+  const query = db
+    .select({
+      titles: animeTable.titles,
+      images: animeTable.images,
+      type: animeTable.type,
+      rating: animeTable.rating,
+      season: animeTable.season,
+      year: animeTable.year,
+      aired: animeTable.aired,
+      episodes: animeTable.episodes,
+      score: animeTable.score,
+      scored_by: animeTable.scored_by,
+      rank: animeTable.rank,
+      genres: animeTable.genres,
+      mal_id: animeTable.mal_id,
+      status: animeTable.status,
+      entityStatus: trackedEntityTable.entityStatus,
+    })
+    .from(animeTable)
+    .where(where)
+    .offset(offset)
+    .limit(limit)
+    .leftJoin(
+      trackedEntityTable,
+      eq(animeTable.mal_id, trackedEntityTable.mal_id),
+    );
+  let stringifiedAnimeRecords: any[] = [];
+  try {
+    if (orderBy) {
+      stringifiedAnimeRecords = await query.orderBy(orderBy);
+    }
+    stringifiedAnimeRecords = await query;
+    return ok(
+      stringifiedAnimeRecords.map((anime) =>
+        parseRecord(anime, stringifiedAnimeKeys),
+      ),
+    );
+  } catch (_) {
+    console.error(_);
+    return err(new Error("Could not get animes"));
+  }
+}
+
+export async function getAnimesWithStatusCount(
+  status: string,
+  searchParams: URLSearchParams,
+  recordsPerPage = 25,
+): Promise<Result<number, Error>> {
+  const cleanedSearchParams = cleanSearchParams(searchParams, animeFilters);
+  let { where, orderBy } = animeSearchParamsToDrizzleQuery(
+    cleanedSearchParams,
+    recordsPerPage,
+  );
+  where = and(where, eq(trackedEntityTable.entityStatus, status));
+  const query = db
+    .select({ count: count() })
+    .from(animeTable)
+    .where(where)
+    .leftJoin(
+      trackedEntityTable,
+      eq(animeTable.mal_id, trackedEntityTable.mal_id),
+    );
+  let animesCount: number;
+  try {
+    if (orderBy) {
+      animesCount = (await query.orderBy(orderBy))[0]?.count as number;
+    }
+    animesCount = (await query)[0]?.count as number;
+    return ok(animesCount);
+  } catch (_) {
+    console.error(_);
+    return err(new Error("Could not get animes count"));
+  }
+}
+
+export async function getAnimesWithStatus(
+  status: string,
+  searchParams: URLSearchParams,
+  recordsPerPage = 25,
+): Promise<Result<AnimeCardItem[], Error>> {
+  const cleanedSearchParams = cleanSearchParams(searchParams, animeFilters);
+  let { where, orderBy, offset, limit } = animeSearchParamsToDrizzleQuery(
+    cleanedSearchParams,
+    recordsPerPage,
+  );
+  where = and(where, eq(trackedEntityTable.entityStatus, status));
   const query = db
     .select({
       titles: animeTable.titles,
