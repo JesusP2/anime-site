a510d82b88d297ccc7759939b9d376932bbda79a
JesusP2 <jesus_perez99@hotmail.com>
2025-02-09 03:20:32 -0600

    added dropdown

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -6,6 +6,15 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+} from "@/components/ui/select";
+import * as SelectPrimitive from "@radix-ui/react-select";
+import { buttonVariants } from "./ui/button";
+import { cn } from "@/lib/utils";
 
 export function Pagination({
   currentPage,
@@ -42,12 +51,40 @@ export function Pagination({
         </PaginationItem>
         {tabs.map((tab, idx) => (
           <PaginationItem key={tab + idx.toString()}>
-            <PaginationLink
-              href={createLink(url, tab, lastVisiblePage)}
-              isActive={tab === currentPage}
-            >
-              {tab}
-            </PaginationLink>
+            {tab === '...' ? (
+              <Select onValueChange={(value) => window.location.href = createLink(url, value, lastVisiblePage)}>
+                <SelectPrimitive.Trigger
+                  className={cn(buttonVariants({ variant: 'ghost' }), 'w-9 p-0')}
+                >
+                  ...
+                </SelectPrimitive.Trigger>
+                <SelectContent>
+                  {Array.from(
+                    { length: lastVisiblePage },
+                    (_, i) => i + 1
+                  ).filter(num => {
+                    const prevTabValue = tabs[idx - 1];
+                    const nextTabValue = tabs[idx + 1];
+                    if (idx < currentPage && typeof prevTabValue === 'number' && typeof nextTabValue === 'number') {
+                      return num > prevTabValue && num < nextTabValue;
+                    } else if (typeof prevTabValue === 'number' && typeof nextTabValue === 'number') {
+                      return num > prevTabValue && num < nextTabValue;
+                    }
+                  }).map((pageNum) => (
+                    <SelectItem key={pageNum} value={pageNum.toString()}>
+                      {pageNum}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+            ) : (
+              <PaginationLink
+                href={createLink(url, tab, lastVisiblePage)}
+                isActive={tab === currentPage}
+              >
+                {tab}
+              </PaginationLink>
+            )}
           </PaginationItem>
         ))}
         <PaginationItem>
