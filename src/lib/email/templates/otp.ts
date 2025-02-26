4806a8d82b6d08a28dcf59704ef29a1eda93b7e9
JesusP2 <jesus_perez99@hotmail.com>
2025-02-26 03:36:44 -0600

    updated email templates

--- src/lib/email/templates/otp.ts
+++ src/lib/email/templates/otp.ts
@@ -105,104 +105,6 @@ export const forgotPasswordTemplate = (url: string) => `
   <body style="word-spacing: normal; background-color: #f2f2f2">
     <div style="background-color: #f2f2f2" lang="und" dir="auto">
       <!--[if mso | IE]><table align="center" border="0" cellpadding="0" cellspacing="0" class="" role="presentation" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
-
-      <div style="margin: 0px auto; max-width: 600px">
-        <table
-          align="center"
-          border="0"
-          cellpadding="0"
-          cellspacing="0"
-          role="presentation"
-          style="width: 100%"
-        >
-          <tbody>
-            <tr>
-              <td
-                style="
-                  direction: ltr;
-                  font-size: 0px;
-                  padding: 0;
-                  text-align: center;
-                "
-              >
-                <!--[if mso | IE]><table role="presentation" border="0" cellpadding="0" cellspacing="0"><tr><td class="" style="vertical-align:top;width:600px;" ><![endif]-->
-
-                <div
-                  class="mj-column-per-100 mj-outlook-group-fix"
-                  style="
-                    font-size: 0px;
-                    text-align: left;
-                    direction: ltr;
-                    display: inline-block;
-                    vertical-align: top;
-                    width: 100%;
-                  "
-                >
-                  <table
-                    border="0"
-                    cellpadding="0"
-                    cellspacing="0"
-                    role="presentation"
-                    style="vertical-align: top"
-                    width="100%"
-                  >
-                    <tbody>
-                      <tr>
-                        <td
-                          align="center"
-                          style="
-                            font-size: 0px;
-                            padding: 0;
-                            word-break: break-word;
-                          "
-                        >
-                          <table
-                            border="0"
-                            cellpadding="0"
-                            cellspacing="0"
-                            role="presentation"
-                            style="
-                              border-collapse: collapse;
-                              border-spacing: 0px;
-                            "
-                          >
-                            <tbody>
-                              <tr>
-                                <td style="width: 600px">
-                                  <img
-                                    alt="AniSearch"
-                                    src="https://via.placeholder.com/600x80/232f3e/ffffff?text=Anime+App"
-                                    style="
-                                      border: 0;
-                                      display: block;
-                                      outline: none;
-                                      text-decoration: none;
-                                      height: auto;
-                                      width: 100%;
-                                      font-size: 13px;
-                                    "
-                                    width="600"
-                                    height="auto"
-                                  />
-                                </td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-
-                <!--[if mso | IE]></td></tr></table><![endif]-->
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-
-      <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" role="presentation" style="width:600px;" width="600" bgcolor="#ffffff" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
-
       <div
         style="
           background: #ffffff;
@@ -270,7 +172,7 @@ export const forgotPasswordTemplate = (url: string) => `
                               color: #232f3e;
                             "
                           >
-                            Verify your identity
+                            Reset Password
                           </div>
                         </td>
                       </tr>
@@ -290,33 +192,6 @@ export const forgotPasswordTemplate = (url: string) => `
                               font-size: 14px;
                               line-height: 20px;
                               text-align: left;
-                              color: #232f3e;
-                            "
-                          >
-                            Hello, <br /><br />
-                            We received a login request for your account. If you
-                            initiated this request, please use the following
-                            magic link to securely access your account:
-                          </div>
-                        </td>
-                      </tr>
-
-                      <tr>
-                        <td
-                          align="center"
-                          style="
-                            font-size: 0px;
-                            padding: 10px 25px;
-                            word-break: break-word;
-                          "
-                        >
-                          <div
-                            style="
-                              font-family: Arial, sans-serif;
-                              font-size: 18px;
-                              font-weight: bold;
-                              line-height: 1;
-                              text-align: center;
                               color: #232f3e;
                             "
                           >
@@ -383,7 +258,7 @@ export const forgotPasswordTemplate = (url: string) => `
                                     "
                                     target="_blank"
                                   >
-                                    Login Securely
+                                    Reset Password
                                   </a>
                                 </td>
                               </tr>
