e2b4494ba78ebd0f09be9a6f4fc58f6ae51f852e
JesusP2 <jesus_perez99@hotmail.com>
2025-03-10 01:33:32 -0600

    asdj,sadsa

--- src/lib/db/pool.ts
+++ src/lib/db/pool.ts
@@ -1,20 +1,9 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-import { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import * as schema from "./schemas";
 import postgres from "postgres";
 import { POSTGRES_URL } from "astro:env/server";
-
-type DbType = PostgresJsDatabase<typeof schema>;
-
-declare global {
-  var db: DbType | undefined;
-}
 export function getDb() {
   const client = postgres(POSTGRES_URL);
-  if (globalThis.db) {
-    return globalThis.db;
-  }
   const db = drizzle(client, { schema });
-  globalThis.db = db;
   return db;
 }
