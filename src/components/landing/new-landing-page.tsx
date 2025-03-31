b0b56e14c36a1362bd6bee95ebd30e45db219d57
JesusP2 <jesus_perez99@hotmail.com>
2025-03-31 14:16:03 -0600

    yo

--- src/components/landing/new-landing-page.tsx
+++ src/components/landing/new-landing-page.tsx
@@ -1,62 +1,11 @@
-import { useEffect, useRef, useState } from "react";
-import { Button } from "../ui/neobrutalism/button";
 import { ThemeButton } from "../theme-button";
+import { Button } from "../ui/neobrutalism/button";
 
-const WINDOW_WIDTH = 1900;
-const WINDOW_HEIGHT = 897;
-const VIDEO_WIDTH = 360;
-const VIDEO_HEIGHT = 528;
-const VIDEO_X = 500;
-const VIDEO_Y = 300;
 export function NewLandingPage(props: { isDarkMode: boolean }) {
-  const [isDarkMode, setIsDarkMode] = useState(props.isDarkMode);
-  const videoRef = useRef<HTMLVideoElement>(null);
-  const [showVideo, setShowVideo] = useState(false);
-  useEffect(() => {
-    function handleResize() {
-      if (!videoRef.current) return;
-      function getAspectRatio(base: number) {
-        if (base < 0.4) {
-          return base * 1.9;
-        } else if (base < 0.5) {
-          return base * 1.8;
-        } else if (base < 0.6) {
-          return base * 1.5;
-        }
-        return base;
-      }
-      const base = window.innerWidth / WINDOW_WIDTH;
-      const aspectRatio = getAspectRatio(base);
-      if (base < 0.4) {
-        videoRef.current.width = VIDEO_WIDTH * aspectRatio;
-        videoRef.current.height = VIDEO_HEIGHT * aspectRatio;
-        videoRef.current.style.top = `${450 * aspectRatio}px`;
-        videoRef.current.style.right = `${0 * aspectRatio}px`;
-      } else if (base < 0.5) {
-        videoRef.current.width = VIDEO_WIDTH * aspectRatio;
-        videoRef.current.height = VIDEO_HEIGHT * aspectRatio;
-        videoRef.current.style.top = `${350 * aspectRatio}px`;
-        videoRef.current.style.right = `${0 * aspectRatio}px`;
-      } else if (base < 0.6) {
-        videoRef.current.width = VIDEO_WIDTH * aspectRatio;
-        videoRef.current.height = VIDEO_HEIGHT * aspectRatio;
-        videoRef.current.style.top = `${400 * aspectRatio}px`;
-        videoRef.current.style.right = `${100 * aspectRatio}px`;
-      } else {
-        videoRef.current.width = VIDEO_WIDTH * aspectRatio;
-        videoRef.current.height = VIDEO_HEIGHT * aspectRatio;
-        videoRef.current.style.top = `${VIDEO_Y * aspectRatio}px`;
-        videoRef.current.style.right = `${VIDEO_X * aspectRatio}px`;
-      }
-      setShowVideo(true);
-    }
-    handleResize();
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
   return (
     <>
       <section className="h-screen">
+        <ThemeButton isDarkMode={props.isDarkMode} />
         <div className="grid place-items-center space-y-10 px-10 :sm:px-20 md:px-28 lg:px-40 pt-10 sm:pt-20 md:pt-28 lg:pt-40">
           <h1 className="stroked-text font-crimson text-center text-4xl sm:text-5xl lg:text-7xl font-black max-w-4xl z-10">
             Search and track your favorite anime
@@ -71,19 +20,6 @@ export function NewLandingPage(props: { isDarkMode: boolean }) {
             </Button>
           </div>
         </div>
-        {!isDarkMode && (
-          <video ref={videoRef} autoPlay loop muted width="360" height="528" className="absolute" style={{
-            top: `${VIDEO_Y}px`,
-            right: `${VIDEO_X}px`,
-          }}>
-            {showVideo && (
-              <>
-                <source src="/girl-animation.webm" type="video/mp4" />
-                Your browser does not support the video tag.
-              </>
-            )}
-          </video>
-        )}
       </section>
     </>
   )
