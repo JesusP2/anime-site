c5d7e65c1270125b4d08f3546e8ba0f6eb1a3793
JesusP2 <jesus_perez99@hotmail.com>
2025-03-31 15:10:31 -0600

    utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,28 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export const idxToMonth = [
+  "JAN",
+  "FEB",
+  "MARCH",
+  "APRIL",
+  "MAY",
+  "JUNE",
+  "JULY",
+  "AUG",
+  "SEPT",
+  "OCT",
+  "NOV",
+  "DEC",
+];
+
+export function objectKeys<T extends Record<string, unknown>>(obj: T) {
+  return Object.keys(obj) as (keyof T)[];
+}
+export function objectEntries<T extends Record<string, unknown>>(obj: T) {
+  return Object.entries(obj) as [keyof T, T[keyof T]][];
 }
