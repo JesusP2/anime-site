24360f3bcead89216fe462d0675c6535ec14ed0d
JesusP2 <jesus_perez99@hotmail.com>
2025-04-22 23:48:14 -0600

    another test

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -82,6 +82,7 @@ function SidebarProvider({
         _setOpen(openState);
       }
 
+      console.log('saving cookie with value: ', openState);
       // This sets the cookie to keep the sidebar state.
       document.cookie = `${SIDEBAR_COOKIE_NAME}=${openState}; path=/; max-age=${SIDEBAR_COOKIE_MAX_AGE}`;
     },
