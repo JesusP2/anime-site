601564d4280ef2be280a66aa9b70685b6f5c43b3
JesusP2 <jesus_perez99@hotmail.com>
2025-05-06 03:06:52 -0600

    fixed name

--- apps/worker/src/api.ts
+++ apps/worker/src/api.ts
@@ -5,7 +5,7 @@ export const api = new Hono<{ Bindings: Bindings }>().get(
   "/counter",
   async (c) => {
     const env = c.env;
-    const id = env.GAME.idFromName("counter");
+    const id = env.GAME.idFromName("GAME");
     const stub = env.GAME.get(id);
     stub.increment();
     const counterValue = await stub.getCounterValue();
