8097776e0701540d33c69eb07fe8bdb69782d6d0
JesusP2 <jesus_perez99@hotmail.com>
2025-05-07 20:13:06 -0600

    removed counter endpoint

--- apps/worker/src/api.ts
+++ apps/worker/src/api.ts
@@ -4,14 +4,6 @@ import { zValidator } from "@hono/zod-validator";
 import { z } from "zod";
 
 export const api = new Hono<{ Bindings: Bindings }>()
-  .get("/counter", async (c) => {
-    const env = c.env;
-    const id = env.GAME.idFromName("GAME");
-    const stub = env.GAME.get(id);
-    stub.increment();
-    const counterValue = await stub.getCounterValue();
-    return c.text(counterValue.toString());
-  })
   .get(
     "/ws",
     zValidator("query", z.object({ matchId: z.string() })),
